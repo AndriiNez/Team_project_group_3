ef18b0e86d56304e4c486c0c9d971fe7d06c2775
AndriiNez <andreynezen@gmail.com>
2023-09-01 18:09:48 +0200

    додано новий клас

--- src/main/java/org/group_3/GameLogic.java
+++ src/main/java/org/group_3/GameLogic.java
@@ -1,16 +1,6 @@
 package org.group_3;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
